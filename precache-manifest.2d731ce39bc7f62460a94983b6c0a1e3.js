b202fa7f09352db5db9f0ed648eda7bb0fbd8254
g_mohit <g_mohit@lainnovatorslab.com>
2022-06-13 13:12:23 +0530

    Updates

---
+++ precache-manifest.2d731ce39bc7f62460a94983b6c0a1e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b414e30be6287bf6b31a63c71e04b9",
+    "url": "/react-proxy/index.html"
+  },
+  {
+    "revision": "7b25ab45067dcb59145e",
+    "url": "/react-proxy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cdf64ecd502c4d8c0344",
+    "url": "/react-proxy/static/css/main.7071ae30.chunk.css"
+  },
+  {
+    "revision": "7b25ab45067dcb59145e",
+    "url": "/react-proxy/static/js/2.ee0d57f5.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/react-proxy/static/js/2.ee0d57f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf64ecd502c4d8c0344",
+    "url": "/react-proxy/static/js/main.b0068801.chunk.js"
+  },
+  {
+    "revision": "913de29edfe793c74f60",
+    "url": "/react-proxy/static/js/runtime-main.c2cade7b.js"
+  }
+]);
